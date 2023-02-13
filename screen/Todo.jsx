a0cb4b596ce31d2edb4b23a2110f6ae739006048
AkitoshiI <116410941+AkitoshiI@users.noreply.github.com>
2023-02-13 21:57:00 +0900

    最新版更新

--- screen/Todo.jsx
+++ screen/Todo.jsx
@@ -2,6 +2,33 @@ import React from "react";
 import "./todo.css";
 
 export const Todo = (props) => {
+  /*マウスストーカテスト*/
+  /*
+  const mauseStalker = {
+    down(event) {
+      window.addEventListener("mousemove", mauseStalker.move);
+      window.addEventListener("mauseup", mauseStalker.up);
+      console.log(event);
+      event.target.classList.add("active");
+    },
+    move(event) {
+      event.target.style.left = `${event.clientX}px`;
+      event.target.style.top = `${event.clientY}px`;
+    },
+    up(event) {
+      window.removeEventListener("mousemove", mauseStalker.move);
+      window.removeEventListener("mouseup", mauseStalker.up);
+      event.target.classList.remove("active");
+    },
+    {(() => {
+      document.querySelectorAll(".todoContainer").forEach((el) => {
+        el.addEventListener("mousedown", mauseStalker.down);
+      });
+    })()}
+  };
+*/
+  /*マウスストーカテスト*/
+
   //todoリストの描写
   const todoList = props.todo.map((index) => {
     return (
@@ -15,5 +42,5 @@ export const Todo = (props) => {
     );
   });
 
-  return <div>{todoList}</div>;
+  return <div id="list">{todoList}</div>;
 };
